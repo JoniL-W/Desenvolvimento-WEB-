9c9de83259d8d6a0c94f1f8ea9adb37ab1cc78f1
JoniL-W <jonathan.wiessner@unidavi.edu.br>
2025-10-02 21:38:59 -0300

    arquivos adicionados

---
+++ testes/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('funcoes.php');
+
+$media = calcularMedia($notas);
+$statusNota = verificarAprovacaoNota($media);
+
+$frequencia = calcularFrequencia($faltas);
+$statusFrequencia = verificarAprovacaoFrequencia($frequencia);
+
+echo "Média das notas: " . number_format($media, 2, ',', '.') . "<br>";
+echo "Status por nota: $statusNota<br>";
+echo "Frequência: " . number_format($frequencia, 2, ',', '.') . "%<br>";
+echo "Status por frequência: $statusFrequencia<br>";
+?>
